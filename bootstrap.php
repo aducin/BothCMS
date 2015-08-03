de63fd1e47af2f762a667e6b333bb69e6332d1ec
aducin <albert.ducin@gmail.com>
2015-08-03 21:19:47 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/abstractHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/NewHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/OldHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/abstractOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/NewOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/OldOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/abstractProduct.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/OldProduct.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/NewProduct.class.php';
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/HelperClass/abstractHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/HelperClass/NewHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/HelperClass/OldHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/OrderClass/abstractOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/OrderClass/NewOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/OrderClass/OldOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/ProductClass/abstractProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/ProductClass/OldProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/ProductClass/NewProduct.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/prestapdo.php';
