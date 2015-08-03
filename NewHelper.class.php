2f24a3ba838e3d0488a9d0bd158b11de20f63c45
aducin <albert.ducin@gmail.com>
2015-08-03 20:29:59 +0200

    Create NewHelper.class.php

---
+++ NewHelper.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class NewHelper extends Helper{
+
+	protected function selectWhere() {
+		return " WHERE id_category NOT IN (1,2,10,11,12,14, 15, 19, 20, 21,22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 38, 39, 40, 42,43,46,47,52,53)";
+	}
+
+	function __destruct(){
+	}
+}
