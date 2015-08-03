2d75044abdb0abde623f889915aadc9701f3273f
aducin <albert.ducin@gmail.com>
2015-08-03 20:37:04 +0200

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/abstractHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/NewHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/OldHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/abstractOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/NewOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/OldOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/abstractProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/OldProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/NewProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/prestapdo.php';
