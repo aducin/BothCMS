d028107aec87c125735e750373eadaa71910b272
aducin <albert.ducin@gmail.com>
2015-08-03 20:35:58 +0200

    Create OldProduct.class.php

---
+++ OldProduct.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class OldProduct extends Product
+{
+	protected function getWhereProduct() {
+		return " WHERE ps_product_lang.id_lang=3 AND ps_product_lang.id_product= :id_number";
+	}
+
+	protected function getWhereProduct2() {
+		return " WHERE ps_category_lang.id_lang=3 AND ps_category_product.id_product= :id";
+	}
+
+	protected function getWhereProduct3() {
+		return " WHERE id_lang=3 AND id_category NOT IN (1,6)";
+	}
+
+	public function countReduction($price, $reduction) {
+		$new0=number_format($price, 2,'.','');
+		$new=floatval($reduction);
+		$new2=number_format($new, 2,'.','');
+		$oldQueryResult1=$new0-$new;
+		$oldQueryResult2=number_format($oldQueryResult1, 2,'.','');
+		return$oldQueryResult2.'zł<br>W tym rabat: <b>'.$new2.'zł</b>';
+	}
+}
