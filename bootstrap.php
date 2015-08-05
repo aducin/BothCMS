7bb158d35a2ec48af6250afe63980ddaa98a9708
aducin <albert.ducin@gmail.com>
2015-08-05 21:28:11 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/HelperClass/abstractHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/HelperClass/NewHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/HelperClass/OldHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/OrderClass/abstractOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/OrderClass/NewOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/OrderClass/OldOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/ProductClass/abstractProduct.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/ProductClass/OldProduct.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/Classes/ProductClass/NewProduct.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/database.ini';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/abstractHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/NewHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/OldHelper.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/OrderClass/abstractOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/OrderClass/NewOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/OrderClass/OldOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/abstractProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/OldProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/NewProduct.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/DataBaseClass/db.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/config/databaseToDelete.php';
