dcbff34303db05e8268a918ace71aff0af48d29e
aducin <albert.ducin@gmail.com>
2015-08-05 21:20:52 +0200

    Update OldOrder.class.php

--- OldOrder.class.php
+++ OldOrder.class.php
@@ -1,17 +1,8 @@
 <?php
 
-class OldOrder extends Order
+class LinuxPlOrder extends Order
 {
-	protected function getWhereStatement() {
-		return " WHERE ps_product_lang.id_lang=3 AND ps_order_detail.id_order = :id_number";
-	}
-
-	public function getCount($orderId, $pdo){
-		$sql= 'SELECT COUNT(product_name) FROM ps_order_detail 
-		WHERE id_order= :id';
-		$s=$pdo->prepare($sql);
-		$s->bindValue(':id', $orderId);
-		$s->execute();
-		return $s;
+	protected function getWhereSubquery() {
+		return " WHERE id_order = :id_number";
 	}
 }
