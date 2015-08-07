c693369c1d8fbf85146d49a3ad88fc40ccbaf45a
aducin <albert.ducin@gmail.com>
2015-08-07 17:58:31 +0200

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,11 +3,10 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/abstractHelper.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/NewHelper.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/OldHelper.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/OrderClass/abstractOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/OrderClass/NewOrder.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/OrderClass/OldOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/orderClass/abstractOrderClass.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/orderClass/NewOrder.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/orderClass/OldOrder.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/abstractProduct.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/OldProduct.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/NewProduct.class.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/DataBaseClass/db.class.php';
-include_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/config/databaseToDelete.php';
