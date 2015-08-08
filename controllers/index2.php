f39a558019102e91e9526a221a31d412d20ae424
Albert Ducin <albert.ducin@gmail.com>
2015-08-08 19:33:49 +0200

    ...

--- controllers/index2.php
+++ controllers/index2.php
@@ -36,7 +36,114 @@ if (!isset($_SESSION['log'])){
 	}
 }
 include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/orderSearch.html';
+if(isset($_GET['shipmentNumber'])){
+	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/shipmentMail.html';
+	exit();
+}
+if(isset($_GET['action'])&&$_GET['action']=='orderSearch'){
+	if ($_GET['neworder'] !=''){
+		$order2= new LinuxPlOrder($firstHost, $firstLogin, $firstPassword);
+		$query = $order2->getQuery($_GET['neworder']);
 
+		foreach ($query as $sOrder){
+			$this[]=array('id'=>$sOrder['product_id'], 'name'=>$sOrder['name'], 'onStock'=>$sOrder['product_quantity'], 'quantity'=>$sOrder['quantity']);
+		}
+	}
+	if ($_GET['oldorder'] !=''){
+		$order1= new OgicomOrder($secondHost, $secondLogin, $secondPassword);
+		$query = $order1->getQuery($_GET['oldorder']);
+
+		foreach ($query as $sOrder){
+			$this[]=array('id'=>$sOrder['product_id'], 'name'=>$sOrder['name'], 'onStock'=>$sOrder['product_quantity'], 'quantity'=>$sOrder['quantity']);
+		}
+	}
+	if ($_GET['notification'] !=''){
+		if(isset($_GET['send'])&&$_GET['send']=='modele'){
+			$order1= new OgicomOrder($secondHost, $secondLogin, $secondPassword);
+			$notification = $order1->sendNotification($_GET['notification']);
+			$notificationresult = $notification->fetch();
+		}
+		if(isset($_GET['send'])&&$_GET['send']=='ad9bis'){
+			$order2= new LinuxPlOrder($firstHost, $firstLogin, $secondPassword);
+			$notification = $order2->sendNotification($_GET['notification']);
+			$notificationresult = $notification->fetch();
+		}
+	}
+	if ($_GET['detailorder'] !=''){
+		$order1= new OgicomOrder($secondHost, $secondLogin, $secondPassword);
+		$details = $order1->getQueryDetails($_GET['detailorder']);
+		$detailsCount=$order1->getCount($_GET['detailorder']);
+		$detailsCountresult = $detailsCount->fetch();
+		$count = $detailsCountresult[0];
+		foreach ($details as $sDetail){
+			$detail2[]=array('id'=>$sDetail['product_id'], 'name'=>$sDetail['name'], 'price'=>$sDetail['product_price'], 'reduction'=>$sDetail['reduction_amount'], 'quantity'=>$sDetail['product_quantity'], 'total'=>$sDetail['total_price_tax_incl'], 'productSum'=>$sDetail['total_products'], 'totalPaid'=>$sDetail['total_paid'], 'mail'=>$sDetail['email'], 'first'=>$sDetail['firstname'], 'last'=>$sDetail['lastname'], 'reference'=>$sDetail['reference'], 'payment'=>$sDetail['payment']);
+		}
+	}
+	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/orders.html';
+}
+if(isset($_GET['shortEdition'])){
+	try{
+		$product1= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
+		$Query = $product1->getProductQuery($_GET['id']);
+		$QueryResult = $Query->fetch();
+		$Query3= $product1->getReduction($_GET['id']);
+		$QueryResult3 = $Query3->fetch();
+		$product2= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
+		$Query2 = $product2->getProductQuery($_GET['id']);
+		$QueryResult2 = $Query2->fetch();
+		$Query4= $product2->getReduction($_GET['id']);
+		$QueryResult4 = $Query4->fetch();
+		$button= 'Aktualizuj produkt w obu bazach';
+		$editForm='?editformBoth';
+	}catch (PODException $e){
+		echo 'Błąd przy pobieraniu informacji o produkcie: ' . $e->getMessage();
+		exit();
+	}
+	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/form.html.php';
+	exit();
+}
+if(isset($_GET['fullEditionN'])OR(isset($_GET['fullEditionO']))){
+	if (isset($_GET['fullEditionN'])){
+		$product1= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
+		$product2= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
+		$editForm='?editcompleteformnew';
+	}elseif(isset($_GET['fullEditionO'])){
+		$product1= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
+		$product2= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
+		$editForm='?editcompleteformold';
+	}
+	$Query = $product1->getWholeDetailsQuery($_GET['id']);
+	$QueryResult = $Query->fetch();
+	$Query1 = $product1->getReduction($_GET['id']);
+	$reduction1 = $Query1->fetch();
+	$manufacturer = $product1->selectManufacturer($_GET['id']);
+	$category = $product1->getCategory($_GET['id']);
+	foreach ($category as $category1){
+		$this[]=array('id'=>$category1['id_category'], 'name'=>$category1['meta_title']);
+		$selectedCats[]=$category1['id_category'];
+	}
+	$result= $product1->getEveryCategory();
+	foreach ($result as $result2){
+		$this2[]=array('id'=>$result2['id_category'], 'name'=>$result2['meta_title'], 'selected'=> in_array($result2['id_category'], $selectedCats));
+	}
+	$selectTag = $product1->selectTag($_GET['id']);
+	foreach ($selectTag as $tag){
+		if($tag!=''){
+			$this3[]=array('id'=>$tag['id_tag'],'name'=>$tag['name']);
+		}else{
+			$tag='';
+		}
+	}
+	foreach ($this3 as $this4){
+		$tagNames[]=$this4['name']; 
+		$completeTagNames=implode(", ", $tagNames);
+	}
+	$secondPrice = $product2->getPrice($_GET['id']);
+	$reduction = $product2->getReduction($_GET['id']);
+	$reduction2 = $reduction->fetch();
+	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/completeForm.html.php';
+	exit();
+}
 if(isset($_GET['editformBoth'])){
 	if ($_POST['text']==''){
 		echo 'Brak aktualnego wpisu: <b>nazwa produktu!</b>';
@@ -66,21 +173,21 @@ if(isset($_GET['editformBoth'])){
 	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/confirmation.html.php';
 	exit();
 }
-if(isset($_GET['editcompleteformnew'])){
-	$product1= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
-	$product2= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
-	if (isset($_POST['change'])and $_POST['change']== "nameChange"){
-		$oldQuery = $product2->insertModyfy($_POST['id'], $_POST['text']);
-	}
-}
-if(isset($_GET['editcompleteformold'])){
-	$product2= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
-	$product1= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
-	if (isset($_POST['change'])and $_POST['change']== "nameChange"){
-		$oldQuery = $product1->insertModyfy($_POST['id'], $_POST['text']);
-	}
-}
 if(isset($_GET['editcompleteformnew'])OR(isset($_GET['editcompleteformold']))){
+	if(isset($_GET['editcompleteformnew'])){
+		$product1= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
+		$product2= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
+		if (isset($_POST['change'])and $_POST['change']== "nameChange"){
+			$oldQuery = $product2->insertModyfy($_POST['id'], $_POST['text']);
+		}
+	}
+	if(isset($_GET['editcompleteformold'])){
+		$product2= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
+		$product1= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
+		if (isset($_POST['change'])and $_POST['change']== "nameChange"){
+			$oldQuery = $product1->insertModyfy($_POST['id'], $_POST['text']);
+		}
+	}
 	if ($_POST['text']==''){
 		echo 'Musisz podać nazwę produktu!';
 		exit();
@@ -167,51 +274,6 @@ if(isset($_GET['editcompleteformnew'])OR(isset($_GET['editcompleteformold']))){
 	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/confirmation.html.php';
 	exit();
 }
-if (isset($_GET['shipmentNumber'])){
-	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/shipmentMail.html';
-	exit();
-}
-if(isset($_GET['action'])&&$_GET['action']=='orderSearch'){
-	if ($_GET['neworder'] !=''){
-		$order2= new LinuxPlOrder($firstHost, $firstLogin, $firstPassword);
-		$query = $order2->getQuery($_GET['neworder']);
-
-		foreach ($query as $sOrder){
-			$this[]=array('id'=>$sOrder['product_id'], 'name'=>$sOrder['name'], 'onStock'=>$sOrder['product_quantity'], 'quantity'=>$sOrder['quantity']);
-		}
-	}
-	if ($_GET['oldorder'] !=''){
-		$order1= new OgicomOrder($secondHost, $secondLogin, $secondPassword);
-		$query = $order1->getQuery($_GET['oldorder']);
-
-		foreach ($query as $sOrder){
-			$this[]=array('id'=>$sOrder['product_id'], 'name'=>$sOrder['name'], 'onStock'=>$sOrder['product_quantity'], 'quantity'=>$sOrder['quantity']);
-		}
-	}
-	if ($_GET['notification'] !=''){
-		if(isset($_GET['send'])&&$_GET['send']=='modele'){
-			$order1= new OgicomOrder($secondHost, $secondLogin, $secondPassword);
-			$notification = $order1->sendNotification($_GET['notification']);
-			$notificationresult = $notification->fetch();
-		}
-		if(isset($_GET['send'])&&$_GET['send']=='ad9bis'){
-			$order2= new LinuxPlOrder($firstHost, $firstLogin, $secondPassword);
-			$notification = $order2->sendNotification($_GET['notification']);
-			$notificationresult = $notification->fetch();
-		}
-	}
-	if ($_GET['detailorder'] !=''){
-		$order1= new OgicomOrder($secondHost, $secondLogin, $secondPassword);
-		$details = $order1->getQueryDetails($_GET['detailorder']);
-		$detailsCount=$order1->getCount($_GET['detailorder']);
-		$detailsCountresult = $detailsCount->fetch();
-		$count = $detailsCountresult[0];
-		foreach ($details as $sDetail){
-			$detail2[]=array('id'=>$sDetail['product_id'], 'name'=>$sDetail['name'], 'price'=>$sDetail['product_price'], 'reduction'=>$sDetail['reduction_amount'], 'quantity'=>$sDetail['product_quantity'], 'total'=>$sDetail['total_price_tax_incl'], 'productSum'=>$sDetail['total_products'], 'totalPaid'=>$sDetail['total_paid'], 'mail'=>$sDetail['email'], 'first'=>$sDetail['firstname'], 'last'=>$sDetail['lastname'], 'reference'=>$sDetail['reference'], 'payment'=>$sDetail['payment']);
-		}
-	}
-	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/orders.html';
-}
 if (isset($_GET['BPSQO'])OR(isset($_GET['BPSQN']))){
 	if (isset($_GET['BPSQO'])){
 		$order1= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
@@ -226,94 +288,22 @@ if (isset($_GET['BPSQO'])OR(isset($_GET['BPSQN']))){
 	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/confirmation.html.php';
 	exit();
 }
-if(isset($_GET['shortEdition'])){
-	try{
-		$product1= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
-		$Query = $product1->getProductQuery($_GET['id']);
-		$QueryResult = $Query->fetch();
-		$Query3= $product1->getReduction($_GET['id']);
-		$QueryResult3 = $Query3->fetch();
-		$product2= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
-		$Query2 = $product2->getProductQuery($_GET['id']);
-		$QueryResult2 = $Query2->fetch();
-		$Query4= $product2->getReduction($_GET['id']);
-		$QueryResult4 = $Query4->fetch();
-		$button= 'Aktualizuj produkt w obu bazach';
-		$editForm='?editformBoth';
-	}catch (PODException $e){
-		echo 'Błąd przy pobieraniu informacji o produkcie: ' . $e->getMessage();
-		exit();
-	}
-	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/form.html.php';
-	exit();
-}
-if(isset($_GET['fullEditionN'])OR(isset($_GET['fullEditionO']))){
-	if (isset($_GET['fullEditionN'])){
-		$product1= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
-		$product2= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
-		$editForm='?editcompleteformnew';
-	}elseif(isset($_GET['fullEditionO'])){
-		$product1= new OgicomProduct($secondHost, $secondLogin, $secondPassword);
-		$product2= new LinuxPlProduct($firstHost, $firstLogin, $firstPassword);
-		$editForm='?editcompleteformold';
-	}
-	$Query = $product1->getWholeDetailsQuery($_GET['id']);
-	$QueryResult = $Query->fetch();
-	$Query1 = $product1->getReduction($_GET['id']);
-	$reduction1 = $Query1->fetch();
-	$manufacturer = $product1->selectManufacturer($_GET['id']);
-	$category = $product1->getCategory($_GET['id']);
-	foreach ($category as $category1){
-		$this[]=array('id'=>$category1['id_category'], 'name'=>$category1['meta_title']);
-		$selectedCats[]=$category1['id_category'];
-	}
-	$result= $product1->getEveryCategory();
-	foreach ($result as $result2){
-		$this2[]=array('id'=>$result2['id_category'], 'name'=>$result2['meta_title'], 'selected'=> in_array($result2['id_category'], $selectedCats));
-	}
-	$selectTag = $product1->selectTag($_GET['id']);
-	foreach ($selectTag as $tag){
-		if($tag!=''){
-			$this3[]=array('id'=>$tag['id_tag'],'name'=>$tag['name']);
-		}else{
-			$tag='';
-		}
-	}
-	foreach ($this3 as $this4){
-		$tagNames[]=$this4['name']; 
-		$completeTagNames=implode(", ", $tagNames);
-	}
-	$secondPrice = $product2->getPrice($_GET['id']);
-	$reduction = $product2->getReduction($_GET['id']);
-	$reduction2 = $reduction->fetch();
-	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/completeForm.html.php';
-	exit();
-}
-if (isset($_GET['action'])and $_GET['action']== 'Uaktualnij ilości dla całego zamówienia'){
-	try{
+if(isset($_GET['mergeQuantities'])){
+	if($_GET['mergeQuantities']== 'Uaktualnij ilości dla całego zamówienia'){
 		$include=0;
 		$order1= new LinuxPlOrder($secondHost, $secondLogin, $secondPassword);
-		$newQuery = $order1->selectOrderQuantity($_GET['id_number']);
-		foreach ($newQuery as $newQuery2){
-			$mods[]= array('quantity'=>$newQuery2['quantity'], 'product_id'=>$newQuery2['product_id'], 'id_order'=>$newQuery2['id_order']);
-		}
-		include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/orderUpgrade.html.php';
-	}catch (PDOExceptioon $e){
-		echo 'Pobranie ilości w zamówieniu nie powiodło się: ' . $e->getMessage();
-		exit();
-	}
-}
-if (isset($_GET['action'])and $_GET['action']== 'Uaktualnij ilości w całym zamówieniu'){
-	try{
+	}elseif ($_GET['mergeQuantities']== 'Uaktualnij ilości w całym zamówieniu'){
 		$include=1;
-		$order2= new OgicomOrder($firstHost, $firstLogin, $firstPassword);
-		$oldQuery = $order2->selectOrderQuantity($_GET['id_number']);
-		foreach ($oldQuery as $oldQuery2){
-			$mods[]= array('quantity'=>$oldQuery2['quantity'], 'product_id'=>$oldQuery2['product_id'], 'id_order'=>$oldQuery2['id_order']);
+		$order1= new OgicomOrder($firstHost, $firstLogin, $firstPassword);
+	}
+	try{
+		$Query = $order1->selectOrderQuantity($_GET['id_number']);
+		foreach ($Query as $Query2){
+			$mods[]= array('quantity'=>$Query2['quantity'], 'product_id'=>$Query2['product_id'], 'id_order'=>$Query2['id_order']);
 		}
-		include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/orderUpgrade.html.php';
 	}catch (PDOExceptioon $e){
 		echo 'Pobranie ilości w zamówieniu nie powiodło się: ' . $e->getMessage();
 		exit();
 	}
+	include $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/templates/orderUpgrade.html.php';
 }
