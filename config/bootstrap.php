74bdcb6f53cbfeed26598d1aa002d7fb1801405e
Albert Ducin <albert.ducin@gmail.com>
2015-08-10 22:38:20 +0200

    model rename

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/abstractHelper.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/NewHelper.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/OldHelper.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/orderClass/abstractOrderClass.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/orderClass/NewOrder.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/orderClass/OldOrder.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/LinuxPlHelper.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/OgicomHelper.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/OrderClass/abstractOrderClass.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/OrderClass/LinuxPlOrder.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/OrderClass/OgicomOrder.class.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/abstractProduct.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/OldProduct.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/NewProduct.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/OgicomProduct.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/LinuxPlProduct.class.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/DataBaseClass/db.class.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/config/functions.php';
