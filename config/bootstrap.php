236878e718b2c0ce5b65579f7aba6129ac8477cc
Albert Ducin <albert.ducin@gmail.com>
2015-08-10 22:24:00 +0200

    model

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/abstractHelper.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/NewHelper.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/HelperClass/OldHelper.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/orderClass/abstractOrderClass.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/orderClass/NewOrder.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/orderClass/OldOrder.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/abstractProduct.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/OldProduct.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/ProductClass/NewProduct.class.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/classes/DataBaseClass/db.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/abstractHelper.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/NewHelper.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/HelperClass/OldHelper.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/orderClass/abstractOrderClass.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/orderClass/NewOrder.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/orderClass/OldOrder.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/abstractProduct.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/OldProduct.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/ProductClass/NewProduct.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/model/DataBaseClass/db.class.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/Ad9bisCMS/config/functions.php';
